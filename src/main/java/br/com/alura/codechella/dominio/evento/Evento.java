9874e2b99b08f552b775abe6eec4a7d74389e85c
francielle oliveira da luz <francielleoliveiradaluz@Airdefrancielle.Princess.local>
2025-05-06 18:42:40 -0300

    implementacao do DDD

--- src/main/java/br/com/alura/codechella/dominio/evento/Evento.java
+++ src/main/java/br/com/alura/codechella/dominio/evento/Evento.java
@@ -1,24 +1,37 @@
 package br.com.alura.codechella.dominio.evento;
 
-import br.com.alura.codechella.Categoria;
 import br.com.alura.codechella.dominio.ingresso.TipoIngresso;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class Evento {
+    private UUID uuid;
     private Categoria categoria;
     private String descricao;
     private LocalDateTime data;
     private Endereco endereco;
     private List<TipoIngresso> tipoIngressos = new ArrayList<TipoIngresso>();
 
+    public Evento(Categoria categoria, String descricao, LocalDateTime data, Endereco endereco) {
+        this.categoria = categoria;
+        this.descricao = descricao;
+        this.data = data;
+        this.endereco = endereco;
+        gerarIdentificadorUnico();
+    }
+
+    private void gerarIdentificadorUnico() {
+        this.uuid = UUID.randomUUID();
+    }
+
     public void incluiNovoTipoDeIngressoAoEvento(TipoIngresso tipoIngresso) {
         this.tipoIngressos.add(tipoIngresso);
     }
 
-    public void aumentaNumeroDeIngressosPorTipo(){
+    public void aumentaNumeroDeIngressosPorTipo() {
         //implementar
     }
 
---
+++ src/main/java/br/com/alura/codechella/dominio/ingresso/DadosTipoIngresso.java
@@ -0,0 +1,8 @@
+package br.com.alura.codechella.dominio.ingresso;
+
+public record DadosTipoIngresso(
+        Integer codigo,
+        Setor setor,
+        Definicao definicao
+) {
+}
