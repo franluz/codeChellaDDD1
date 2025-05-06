0f042a2e54a5a2361e2da9133308ecd2e5681ee4
francielle oliveira da luz <francielleoliveiradaluz@Airdefrancielle.Princess.local>
2025-05-06 18:53:56 -0300

    tipoingresso

--- src/main/java/br/com/alura/codechella/dominio/evento/Endereco.java
+++ src/main/java/br/com/alura/codechella/dominio/evento/Endereco.java
@@ -4,4 +4,17 @@ public class Endereco {
     private String cep;
     private Integer numero;
     private String complemento;
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
 }
--- src/main/java/br/com/alura/codechella/dominio/ingresso/TipoIngresso.java
+++ src/main/java/br/com/alura/codechella/dominio/ingresso/TipoIngresso.java
@@ -1,4 +1,9 @@
 package br.com.alura.codechella.dominio.ingresso;
 
 public class TipoIngresso {
+    private Setor setor;
+    private String definicao;
+    private Double valor;
+    private Double totalDisponivel;
+
 }
